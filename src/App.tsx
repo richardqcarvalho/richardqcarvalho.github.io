ef09549f9161069e2522431cfb782c0bc06d280e
Richard Carvalho <richardcarvalhooo@gmail.com>
2024-02-28 22:27:59 -0300

    chore: Updating components - Showing a little message while we don't have anything ready

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,9 @@ import './App.css'
 
 function App() {
   return (
-    <div>
-      <span>Testing</span>
+    <div className='message-container'>
+      <span className='message'>Still in progress...</span>
+      <span className='message'>You can see how it is going <a href="https://github.com/richardqcarvalho/richardqcarvalho.github.io">here</a></span>
     </div>
   )
 }
