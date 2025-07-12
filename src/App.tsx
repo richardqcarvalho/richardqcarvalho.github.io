183bcb1f4091b96cc100ae33b892bd08db4288e8
Richard Carvalho <richardcarvalhooo@gmail.com>
2025-07-12 12:56:10 -0300

    feat: Updating packages and using TailwindCSS

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,18 @@
-import './App.css'
-
 function App() {
-  return (
-    <div className='message-container'>
-      <span className='message'>Still in progress...</span>
-      <span className='message'>You can see how it is going <a href="https://github.com/richardqcarvalho/richardqcarvalho.github.io">here</a></span>
-    </div>
-  )
+	return (
+		<div className='bg-black w-full h-full'>
+			<span className='text-white'>Still in progress...</span>
+			<span className='text-white'>
+				You can see how it is going{' '}
+				<a
+					className='text-blue-600 visited:text-purple-600'
+					href='https://github.com/richardqcarvalho/richardqcarvalho.github.io'
+				>
+					here
+				</a>
+			</span>
+		</div>
+	)
 }
 
 export default App
--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,11 @@
+import tailwindcss from '@tailwindcss/vite'
 import react from '@vitejs/plugin-react-swc'
 import { defineConfig } from 'vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  build: {
-    outDir: 'docs'
-  }
+	plugins: [react(), tailwindcss()],
+	build: {
+		outDir: 'docs',
+	},
 })
